4966c1379eb2d8a9bc13bc4f817366c7304ee044
vjordan-cs <vjordan.cs@gmail.com>
2023-03-25 06:10:14 -0500

    api quickstart calls successful

--- pages/quizzes.js
+++ pages/quizzes.js
@@ -1,7 +1,39 @@
 import Head from "next/head";
 import Navigation from "@/components/navigation";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Quizzes() {
+  const [animalInput, setAnimalInput] = useState("");
+  const [result, setResult] = useState();
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    try {
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ animal: animalInput }),
+      });
+
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw (
+          data.error ||
+          new Error(`Request failed with status ${response.status}`)
+        );
+      }
+
+      setResult(data.result);
+      setAnimalInput("");
+    } catch (error) {
+      // Consider implementing your own error handling logic here
+      console.error(error);
+      alert(error.message);
+    }
+  }
   return (
     <>
       <Head>
@@ -12,6 +44,19 @@ export default function Quizzes() {
       </Head>
       <main>
         <Navigation />
+        <div className="h-40"></div>
+        <h3>Name my pet</h3>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="animal"
+            placeholder="Enter an animal"
+            value={animalInput}
+            onChange={(e) => setAnimalInput(e.target.value)}
+          />
+          <input type="submit" value="Generate names" />
+        </form>
+        <div>{result}</div>
       </main>
     </>
   );
